b68762b9455a96fcb9eef3b4f98ba99395df8494
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-07-21 18:34:15 -0400

    completed subtitle

--- src/components/login-page-components/sub_title.js
+++ src/components/login-page-components/sub_title.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
 const sub_title = () => {
+    const subtitle_text = "The place to write down your thoughts, ideas," + 
+                          "and so much more while having the freedom to" + 
+                          "set your journals publicly or privately"
+
     return (
-        <div>
-            
+        <div className="sub-title">
+            <h4>{subtitle_text}</h4>
         </div>
     )
 }
