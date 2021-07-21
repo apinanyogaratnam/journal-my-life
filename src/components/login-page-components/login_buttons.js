71c7c2a862493871e1fcd110bb816f9e2cca6a99
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-07-21 19:56:20 -0400

    removed redundant html tag

--- src/components/login-page-components/login_buttons.js
+++ src/components/login-page-components/login_buttons.js
@@ -3,7 +3,7 @@ import React from 'react'
 const LoginButtons = () => {
     return (
         <div className="button-container">
-            <button><strong>Continue With Google</strong></button>
+            <button>Continue With Google</button>
             <button>Browse Anonymously</button>
         </div>
     )
--- src/components/login-page-components/sub_title.js
+++ src/components/login-page-components/sub_title.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SubTitle = () => {
     const subtitle_text = "The place to write down your thoughts, ideas," + 
-                          "and so much more while having the freedom to" + 
+                          "and so much more while having the freedom to " + 
                           "set your journals publicly or privately"
 
     return (
