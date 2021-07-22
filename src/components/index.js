0594483b719d7932a78b683c9c7dcdfda5f065ce
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-07-22 00:05:45 -0400

    fixed import error

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
-export { default as LoginPage } from './login_page';
+export { default as LoginPage } from './login-page-components/login_page';
 export { default as JournalsHomePage } from './journals_home_page';
+export { default as LoginButtons } from './login-page-components/login_buttons';
