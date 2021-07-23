9df98060fa9af8da825f58cdc4837ac0f0ae8417
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-07-23 16:27:58 -0400

    added post http method for api

--- API/main.py
+++ API/main.py
@@ -31,5 +31,18 @@ def get():
     return jsonify({"data": sample_json_data})
 
 
+post_url = '/api/v1/token=' + api_key
+@app.route(post_url, methods=['POST'])
+def create():
+    obj = {
+        "firstname": "Apinan",
+        "lastname": "Yogaratnam",
+        "username": "apinanyogaratnam",
+        "email": "email@email.com"
+    }
+    sample_json_data.append(obj)
+    return jsonify({"created": sample_json_data})
+
+
 if __name__ == '__main__':
     app.run(debug=False)
