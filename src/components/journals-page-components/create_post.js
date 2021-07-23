17f1c0c9724ad0730e070016c5649b605a297b18
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-07-22 23:15:58 -0400

    completed create post form

--- src/components/journals-page-components/create_post.js
+++ src/components/journals-page-components/create_post.js
@@ -3,7 +3,22 @@ import React from 'react'
 const create_post = () => {
     return (
         <div>
-            <input></input>
+            <h1>Create Post</h1>
+            <form>
+                <div className="input-journal-title-container">
+                    <input placeholder="Title" className="input-journal-title"></input>
+                </div>
+                <div className="input-journal-title-container">
+                    <textarea placeholder="Once upon a time..." className="input-journal-text"></textarea>
+                </div>
+                <div className="checkbox-container"> 
+                    <label>
+                    Private Post&nbsp;
+                    <input type="checkbox"></input>
+                    </label>
+                </div>
+                <button className="submit-button">Post</button>
+            </form>
         </div>
     )
 }
