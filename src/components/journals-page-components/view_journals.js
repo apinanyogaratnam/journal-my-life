9c8b3f9cf9e561b6108e8d73e415706165eb440a
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-07-23 15:02:38 -0400

    completed template of view journals

--- src/components/journals-page-components/view_journals.js
+++ src/components/journals-page-components/view_journals.js
@@ -3,7 +3,29 @@ import React from 'react'
 const view_journals = () => {
     return (
         <div>
-            
+            <h1>Your Journals</h1>
+            <div className="journals-container">
+                <div className="journal-container journal-text">
+                    <h2>Journal Title</h2>
+                    <h3 className="journal-author">- Author</h3>
+                    <h4>This is a sample journal blah blah blah</h4>
+                </div>
+                <div className="journal-container journal-text">
+                    <h2>Journal Title</h2>
+                    <h3 className="journal-author">- Author</h3>
+                    <h4>This is a sample journal blah blah blah</h4>
+                </div>
+                <div className="journal-container journal-text">
+                    <h2>Journal Title</h2>
+                    <h3 className="journal-author">- Author</h3>
+                    <h4>This is a sample journal blah blah blah</h4>
+                </div>
+                <div className="journal-container journal-text">
+                    <h2>Journal Title</h2>
+                    <h3 className="journal-author">- Author</h3>
+                    <h4>This is a sample journal blah blah blah</h4>
+                </div>
+            </div>
         </div>
     )
 }
