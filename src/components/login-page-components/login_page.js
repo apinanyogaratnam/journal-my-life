d648acd9910475a7c76c71350d6175325ea3f867
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-07-22 20:12:01 -0400

    removed unnessary imports from login_page.js

--- src/components/login-page-components/login_page.js
+++ src/components/login-page-components/login_page.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import '../../index.css';
-import { Route } from 'react-router-dom';
-import { LoginButtons, AnonJournalsHomePage, ExistingUserJournalsHomePage, NavBar, Title, SubTitle } from '../index';
+import { LoginButtons, NavBar, Title, SubTitle } from '../index';
 
 const LoginPage = () => {
     return (
