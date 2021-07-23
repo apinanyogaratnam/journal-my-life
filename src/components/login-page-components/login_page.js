cf117a5f17f9c5a8305e0fc63a259f0d6527f87c
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-07-23 12:13:44 -0400

    adding continue with google feature

--- src/components/login-page-components/login_page.js
+++ src/components/login-page-components/login_page.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import '../../index.css';
 import { LoginButtons, NavBar, Title, SubTitle } from '../index';
+import GoogleLogin from 'react-google-login';
 
 const LoginPage = () => {
+    const responseGoogle = (response) => {
+        console.log(response);
+        console.log(response.profileObj);
+    }
+
     return (
         <div>
             <NavBar />
@@ -10,6 +16,13 @@ const LoginPage = () => {
             <SubTitle />
             <hr />
             <LoginButtons />
+            <GoogleLogin 
+                clientId=""
+                buttonText="Continue with Google"
+                onSuccess={responseGoogle}
+                onFailure={responseGoogle}
+                cookiePolicy={'single_host_origin'}
+            />
         </div>
     );
 }
